0ad6d1761d10519805bd0827a0761082d363d06a
Tony Wang <Tony94042@gmail.com>
2023-10-25 17:26:44 +1100

    Added Basic Routing

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, Router } from '@angular/router'
+import { SignInComponent } from "./sign-in/sign-in.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+
+const routes: Routes = [
+    {path:'signup', component: SignUpComponent},
+    {path: '', component: SignInComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
   declarations: [
     SignUpComponent,
-    SignInComponent
+    SignInComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
