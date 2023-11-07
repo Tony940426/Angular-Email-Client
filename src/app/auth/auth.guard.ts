42ebf5a391351ff170cde9c94dd93e984388c41a
Tony Wang <Tony94042@gmail.com>
2023-11-07 12:33:49 +1100

    Completed Guard Feature

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,9 +1,20 @@
-import { CanMatchFn } from '@angular/router';
-import { Observable} from 'rxjs';
+import { CanMatchFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { take, skipWhile, tap, map } from 'rxjs/operators';
+import { inject } from '@angular/core';
 
-export const authGuard(private: authservice: AuthService): CanMatchFn = (route, segments) => {
-  return new Observable((subscriber) => {
-    subscriber.next(true);
-  });
+export const authGuard: CanMatchFn = (route, segments) => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+
+  return authService.signedin$.pipe(
+    skipWhile(value => value === null),
+    take(1),
+    tap((authenticated) => {
+      if(!authenticated){
+        router.navigateByUrl('/');
+      }
+    }),
+    map(authenticated => authenticated)
+  )
 };
