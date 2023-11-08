2fe6bbecacd0c5df51f09be1011f623bba4460ed
Tony Wang <Tony94042@gmail.com>
2023-11-08 17:19:32 +1100

    Added the pages for home

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-index',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class EmailIndexComponent {
 
+  emails = [];
+
+  constructor(private emailService: EmailService){}
+  
+  ngOnInit(){
+    this.emailService.getEmails().subscribe((emails) => {
+      this.emails = emails
+      console.log(this.emails)
+    })
+  }
 }
--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -5,17 +5,16 @@ interface EmailSummary {
   id: string;
   subject: string;
   from: string
-
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
-  rootUrl:'https://api.angular-email.com'
+  rootUrl = 'https://api.angular-email.com'
 
   constructor(private http: HttpClient) {}
   getEmails(){
-    return this.http.get<>(`${this.rootUrl}/emails`)
+    return this.http.get<EmailSummary[]>(`${this.rootUrl}/emails`)
   }
 }
