ca8c448ca0049f2372efe9fe76dd04bc8906929c
Tony Wang <Tony94042@gmail.com>
2023-11-17 17:30:20 +1100

    Function to send email completed, Added margin to button

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Email } from '../email';
 import { AuthService } from 'src/app/auth/auth.service';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-create',
@@ -11,7 +12,7 @@ export class EmailCreateComponent {
   showModal = false;
   email: Email;
 
-  constructor(private authservice: AuthService){
+  constructor(private authservice: AuthService, private emailService: EmailService){
     this.email = {
       id: '',
       to: '',
@@ -22,6 +23,8 @@ export class EmailCreateComponent {
     };
   }
   onSubmit(email: Email){
-    
+      this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false
+    })
   }
 }
